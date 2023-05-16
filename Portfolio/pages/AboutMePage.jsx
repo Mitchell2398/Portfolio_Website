3508990433633ffb860036e0703fae769a4f3e87
Jack Mitchell <jackmitchell2398@gmail.com>
2023-05-16 21:59:37 +0200

    Added content to about us page

--- Portfolio/pages/AboutMePage.jsx
+++ Portfolio/pages/AboutMePage.jsx
@@ -1,14 +1,54 @@
 import React from "react";
-import Header from "../components/Header";
+import Header from "/components/Header";
+
+export default function AboutMePage() {
+  const [aboutMePageData, setAboutMePageData] = React.useState({});
+
+  React.useEffect(() => {
+    fetch("aboutMePageData.json")
+      .then((response) => response.json())
+      .then((data) => setAboutMePageData(data));
+  }, []);
+
+  const { name, email, address, website, summary } = aboutMePageData;
+  const education = aboutMePageData.education && aboutMePageData.education[0];
+  const skills = aboutMePageData.skills && aboutMePageData.skills[0];
+
+  return (
+    <div className="aboutMePageContainer">
+      <Header />
+      <h2 className="aboutMePageTitle">Introduction</h2>
+      <ul>
+        <li>{name}</li>
+        <li>{email}</li>
+        <li>{address}</li>
+        <li>{website}</li>
+        <li>{summary}</li>
+      </ul>
+      <h2>Education</h2>
+      {education && (
+        <ul>
+          <li>{education.institution}</li>
+          <li>{education.area}</li>
+          <li>{education.studyType}</li>
+          <li>
+            {education.startDate} - {education.endDate}
+          </li>
+          <li>{education.gpa}</li>
+        </ul>
+      )}
+      <h2>Skills</h2>
+      {skills && (
+        <ul>
+            <li>{skills.s1}</li>
+            <li>{skills.s2}</li>
+            <li>{skills.s3}</li>
+            <li>{skills.s4}</li>
+        </ul>
 
 
-export default function AboutMePage () {
-    return (
-        <div className="Container">
-            <Header /> 
-            <h1>Hello</h1>
-        </div>
-    )
+      )}
+
+    </div>
+  );
 }
-
-console.log(window.location.pathname)
--- Portfolio/pages/Home.jsx
+++ Portfolio/pages/Home.jsx
@@ -20,7 +20,7 @@ export default function Home () {
     ))
 
 return (
-    <div className="container">
+    <div>
         <Header />
         <AboutMe
             aboutMe={renderAboutMe} 
