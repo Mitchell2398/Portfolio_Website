0f0a2f8b4fb2256b11d61a5cb22bb8fbbd4db273
Jack Mitchell <jackmitchell2398@gmail.com>
2023-07-08 19:54:16 +0100

    small change

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config
-
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
-    base: "/Portfolio_Website",
+    base: `/Portfolio_Website/`,
     plugins: [react()],
-  }
+    esbuild: {
+      jsxFactory: "React.createElement",
+      jsxFragment: "React.Fragment",
+    },
+  };
 
-
-
-  return config
-})
+  return config;
+});
