d30e775343113fc8ee7355f13e1c7b12d3220065
Jack Mitchell <jackmitchell2398@gmail.com>
2023-07-08 20:58:26 +0100

    changed base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config
 export default defineConfig(() => {
   const config = {
-    base: `https://Mitchell2398.github.io/Portfolio_Website/`,
+    base: `/https://jackmitchellsportfolio.com/`,
     plugins: [react()],
     esbuild: {
       jsxFactory: "React.createElement",
