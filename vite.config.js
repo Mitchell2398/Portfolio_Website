819e5f08c8c258cc67cd7caa476808a860b37f66
Jack Mitchell <jackmitchell2398@gmail.com>
2023-07-08 12:38:50 +0100

    Changed vite.config to ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    jsx: "react",
+  }
+
+  
+
+  if (command !== 'serve') {
+    config.base = '/Portfolio_Website/'
+  }
+
+  return config
 })
