50cbd831b794f1119eb3d3b2c8973eec4e8ad192
Jack Mitchell <jackmitchell2398@gmail.com>
2023-07-08 12:44:51 +0100

    Made small changes to congigure for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => {
   const config = {
+    base: "/Portfolio_Website",
     plugins: [react()],
     base: '/',
     jsx: "react",
