3d2ff73f651a5f737aa6d92b3cc8152f06baafb7
Jack Mitchell <jackmitchell2398@gmail.com>
2023-07-08 19:57:59 +0100

    small changes

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Project from "/components/Project.jsx";
 import ProjectPopUp from "/components/ProjectPopUp.jsx";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 
 export default function Projects(props) {
   const [enablePu, setEnablePu] = useState(false);
