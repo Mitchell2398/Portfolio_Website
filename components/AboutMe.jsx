9264678d3238f8bb4462e809370ba62cf6b1b2fe
Jack Mitchell <jackmitchell2398@gmail.com>
2023-12-02 17:25:39 +0000

    few minor changes to about me section

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -5,14 +5,30 @@ export default function AboutMe(props) {
     <div className="aboutMeWrapper" id="aboutMe">
       <div className="aboutMeContainer">
         <div className="aboutMeIntro">
-          <img className="doodle" src="https://res.cloudinary.com/dheko2ynz/image/upload/v1688845526/Icons_SVG/doodle_bi3mtk.svg" />
+          <img
+            className="doodle"
+            src="https://res.cloudinary.com/dheko2ynz/image/upload/v1688845526/Icons_SVG/doodle_bi3mtk.svg"
+          />
 
           <div className="aboutMeTextWrapper">
-            <h1 className="aboutMeTitle">About me.</h1>
+            <h1 className="aboutMeTitle">Hi, I'm Jack.</h1>
             <div className="aboutMeText">
-            <p>{props.aboutMe.introduction}</p>
-            <p>{props.aboutMe.drive}</p>
-            <p>{props.aboutMe.dedication}</p>
+              <p>
+                I am a self-taught developer with a keen interest in frontend
+                development, a journey I began over two years ago. My education,
+                a blend of YouTube tutorials, reading, and classes, has equipped
+                me with essential skills in HTML, CSS, and JavaScript, along
+                with a familiarity with frameworks like React JS and SASS like Tailwind. <br />
+                
+                <br />My focus has been on creating user-friendly, responsive designs
+                that prioritize user experience. In my projects, I strive to
+                balance functionality with clean, efficient code. I am a
+                problem-solver at heart, always eager to learn new techniques
+                and collaborate with others in the tech community. As I continue
+                to grow in this field, I look forward to contributing to
+                meaningful projects and further developing my skills as a
+                frontend developer.
+              </p>
             </div>
           </div>
         </div>
@@ -26,7 +42,10 @@ export default function AboutMe(props) {
             className="icon"
             src="https://res.cloudinary.com/dheko2ynz/image/upload/v1688845525/Icons_SVG/css3-02-svgrepo-com_ix35tb.svg"
           />
-          <img className="icon" src="https://res.cloudinary.com/dheko2ynz/image/upload/v1688845525/Icons_SVG/js01-svgrepo-com_wtlu0h.svg" />
+          <img
+            className="icon"
+            src="https://res.cloudinary.com/dheko2ynz/image/upload/v1688845525/Icons_SVG/js01-svgrepo-com_wtlu0h.svg"
+          />
           <img
             className="icon"
             src="https://res.cloudinary.com/dheko2ynz/image/upload/v1688845527/Icons_SVG/react-svgrepo-com_fjodmm.svg"
@@ -38,7 +57,7 @@ export default function AboutMe(props) {
         </div>
 
         <div className="aboutMeBtnContainer">
-          <a
+          {/* <a
             className="aboutMeBtn"
             onClick={() =>
               window.open(
@@ -47,12 +66,11 @@ export default function AboutMe(props) {
             }
           >
             View cv
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
   );
 }
 {
-  
 }
